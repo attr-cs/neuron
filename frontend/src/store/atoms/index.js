3984d9f3ed9ded4a2c919396a7184d5d72472fd6
Dhruv Bhardwaj <157011981+attr-cs@users.noreply.github.com>
2024-11-23 22:08:26 +0530

    Update index.js

--- frontend/src/store/atoms/index.js
+++ frontend/src/store/atoms/index.js
@@ -24,73 +24,7 @@ export const themeState = atom({
     default: 'light',
 })
 
-// import { atom } from 'recoil';
+ });
 
 
-// export const authState = atom({
-//   key: 'authState',
-//   default: {
-//     isAuthenticated: false,
-//     userId: null,
-//     token: null,
-//     username: null
-//   }
-// });
 
-
-// export const profileState = atom({
-//   key: 'profileState',
-//   default: {
-//     profileImageUrl: '',
-//     bio: '',
-//     location: '',
-//     websiteUrl: '',
-//     bannerImageUrl: '',
-//     dateJoined: null
-//   }
-// });
-
-
-// export const personalInfoState = atom({
-//   key: 'personalInfoState',
-//   default: {
-//     firstname: '',
-//     lastname: '',
-//     email: ''
-//   }
-// });
-
-
-// export const followersState = atom({
-//   key: 'followersState',
-//   default: []
-// });
-
-// export const followingsState = atom({
-//   key: 'followingsState',
-//   default: []
-// });
-
-
-// export const blockedUsersState = atom({
-//   key: 'blockedUsersState',
-//   default: []
-// });
-
-
-// export const isAdminState = atom({
-//   key: 'isAdminState',
-//   default: false
-// });
-
-
-// export const themeState = atom({
-//   key: 'themeState',
-//   default: 'light'
-// });
-
-
-// export const isOnlineState = atom({
-//   key: 'isOnlineState',
-//   default: false
-// });
