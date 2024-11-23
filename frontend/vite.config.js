1a8c4dc2be2942c566f148f2da9c7d4ef1f9e30e
Dhruv Bhardwaj <157011981+attr-cs@users.noreply.github.com>
2024-11-23 22:06:19 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,19 +11,3 @@ export default defineConfig({
   },
 })
 
-// for replit
-// import path from "path";
-// import react from "@vitejs/plugin-react";
-// import { defineConfig } from "vite";
-
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-//   base: import.meta.env.MODE === 'production' 
-//     ? `/${import.meta.env.VITE_REPLIT_PROJECT_NAME}/`  
-//     : '/', 
-// });
